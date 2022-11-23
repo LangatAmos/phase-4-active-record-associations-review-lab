66df3f3363dfa3a4d54a5e57a56aee78de90f770
Amos Langat <amos.langat@student.moringaschool.com>
2022-11-23 23:41:56 +0300

    complete assignment

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,6 +1,9 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
+      t.string :driver_name
+      t.string :car_make
+      t.string :fleet_number
 
       t.timestamps
     end
--- db/migrate/20210506153121_create_passengers.rb
+++ db/migrate/20210506153121_create_passengers.rb
@@ -1,6 +1,9 @@
 class CreatePassengers < ActiveRecord::Migration[6.1]
   def change
     create_table :passengers do |t|
+      t.string :passenger_name
+      t.string :email
+      t.string :location
 
       t.timestamps
     end
